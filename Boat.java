68a6831b9f4f08b01b89f815670f1109d89b2ba5
BiancaDavey <biancamdavey@icloud.com>
2020-12-28 00:18:24 +0100

    Created BoatUser class to enable user input for creating Boat objects. Updated BoatDemo and Boat.java.

---
+++ Boat.java
@@ -0,0 +1,49 @@
+public class Boat {
+
+    /**
+        This program takes weight, length and height values and calculates whether a given combination of these values would result in a boat sinking with a given number of passengers.
+        @param PW a final integer value representing the density of water per m3.
+        @param PC a final integer value representing the density of the wood material of the boat per m3.
+        @param AVERAGE_WEIGHT a final double value representing the average weight of a passenger.
+        @param width a double value representing the width measurement.
+        @param height a double value representing the height measurement.
+        @param length a double value representing the length measurement.
+
+    */
+
+    final int PW = 1030; 
+    final int PC = 900;  
+    final double AVERAGE_WEIGHT = 75.0; 
+    public double width; 
+    public double height;
+    public double length; 
+
+    /** 
+        The Boat constructor accepts width, height, and length arguments and throws an IllegalArgumentException if any of their values are below zero.
+    */
+
+    public Boat(double w, double h, double l) throws IllegalArgumentException {
+        width = w;
+        height = h;
+        length = l;
+        if ((width < 0) || (height < 0) || (length < 0)) {
+            throw new IllegalArgumentException("Value cannot be below zero.");
+        }
+    }
+
+    /*
+        The castAway method applies a calculation to determine the volume of the boat and the number of passengers that it can hold before sinking.
+        An InsufficientBuoyancyException is thrown if the result of the calculation is less than the MIN_PASSENGERS value.
+        @param result a double value representing the result of the calculation applied to the width, height and length values.
+    */
+
+    public double castAway(int MIN_PASSENGERS) throws InsufficientBuoyancyException{
+        double result;
+        result = ((width * height * length)/AVERAGE_WEIGHT) * (PW-PC);
+        if (result < MIN_PASSENGERS) {
+            throw new InsufficientBuoyancyException();
+        }
+        return result;
+    }
+
+}
---
+++ BoatUser.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BoatUser {
+
+    /**
+     * This class enables the creation of Boat objects through prompting user input.
+     * The castAway method is applied to the Boat object created by the user to calculate the buoyancy.
+     */
+
+    private static final int MIN_PASSENGERS = 36;  // The castAway method will be called with the passenger number being 36.
+    public static void main(String[] args) throws Exception{
+     
+        System.out.print("Boat\n");  
+        try {
+            Scanner keyboard = new Scanner (System.in);
+            System.out.println("Enter the width of the boat: ");
+            double width = keyboard.nextDouble();
+            System.out.println("Enter the length of the boat: ");
+            double length = keyboard.nextDouble();
+            System.out.println("Enter the height of the boat: ");
+            double height = keyboard.nextDouble();
+
+            Boat newBoat = new Boat(width, length, height);
+            newBoat.castAway(MIN_PASSENGERS);
+            System.out.println("The boat cast away with " + MIN_PASSENGERS + " passengers.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("The boat could not cast away: " + e.getMessage());
+        } catch (InsufficientBuoyancyException e) {
+            System.out.println("The boat has sunk: " + e.getMessage());
+        }
+    }
+
+}
